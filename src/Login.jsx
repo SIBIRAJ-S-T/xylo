ed728de478e424b2c0c9fd62070f29afa2657705
SIBIRAJST <22csd040@skcet.ac.in>
2023-11-29 12:00:17 +0530

    yes

--- src/Login.jsx
+++ src/Login.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom'
 export default function Login() {
 
   return (
-    <div>
+    <div className='Loginmain'>
     <div class="login-page">
     <div class="form">
     <form class="login-form">
       <input type="text" placeholder="Username or Email"/>
       <input type="password" placeholder="Password"/>
-      <button><Link to="/">login</Link></button>
+      <button><Link to="/" style={{textDecoration: 'none', color:'inherit'}}>login</Link></button>
       <p class="message">Not registered?
       <Link to="/Signup">Create an account</Link>
       </p>
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import './Loginsignup.css'
 import { Link } from 'react-router-dom'
-//import Login from './Login'
+// import Login from './Login'
 
 export default function Signup() {
   return (
-    <div>
+    <div className='Signupmain'>
     <div class="login-page">
     <div class="form">
     <form class="register-form">
     <input type="text" placeholder="Name"/>
     <input type="password" placeholder="Password"/>
     <input type="text" placeholder="Email"/>
-    <button>Create Account</button>
+    <button><Link to="/" style={{textDecoration: 'none', color:'inherit'}}>Create Account</Link></button>
     <p class="message">Already registered? 
     <Link to="/Login">Sign In</Link></p>
     </form>
