d8a2e427ddf1566cdc2b500bd67bf947fa948835
Sanjay-Bala <727722eucd040@skcet.ac.in>
2023-11-29 12:12:44 +0530

    s

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <div>
     
+    
+    
         <header>
             <h1>My E-commerce Store</h1>
         </header>
@@ -15,24 +17,27 @@ export default function Home() {
         </nav>
     
         <section>
-            <h2>Featured Products</h2>
-    
             <div class="product">
                 <img src="product1.jpg" alt="Product 1"/>
-                <h3>Product Name 1</h3>
-                <p>Description of Product 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <p>$19.99</p>
-                <button>Add to Cart</button>
+                <div class="product-info">
+                    <h3>Product Name 1</h3>
+                    <p>Description of Product 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <p>$19.99</p>
+                    <button>Add to Cart</button>
+                </div>
             </div>
     
             <div class="product">
                 <img src="product2.jpg" alt="Product 2"/>
-                <h3>Product Name 2</h3>
-                <p>Description of Product 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <p>$29.99</p>
-                <button>Add to Cart</button>
+                <div class="product-info">
+                    <h3>Product Name 2</h3>
+                    <p>Description of Product 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <p>$29.99</p>
+                    <button>Add to Cart</button>
+                </div>
             </div>
     
+           
     
         </section>
     
@@ -40,7 +45,7 @@ export default function Home() {
             &copy; 2023 My E-commerce Store. All rights reserved.
         </footer>
     
-    
+   
     
     </div>
   )
