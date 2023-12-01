199f89adc906a274f3f7420beba064eee842c8c9
SIBIRAJST <22csd040@skcet.ac.in>
2023-12-01 09:46:50 +0530

    ug

--- src/Inhome/Inhome8.jsx
+++ src/Inhome/Inhome8.jsx
@@ -1,43 +1,40 @@
-import React from 'react'
-import './Inhome1.css'
-// import Home from './Home';
-export default function Inhome1() {
+import React from 'react';
+import './Inhome1.css';
 
-    const items = [
-        { id: 1, name: 'Item 1', price: 10, img:'image-link'},
-        { id: 1, name: 'Item 1', price: 10, img:'image-link'},
-        { id: 1, name: 'Item 1', price: 10, img:'image-link'},
-        { id: 1, name: 'Item 1', price: 10, img:'image-link'},
-        { id: 1, name: 'Item 1', price: 10, img:'image-link'},
-        { id: 1, name: 'Item 1', price: 10, img:'image-link'},
-        { id: 1, name: 'Item 1', price: 10, img:'image-link'},
-        { id: 1, name: 'Item 1', price: 10, img:'image-link'},
-      ]
+export default function Inhome1() {
+  const items = [
+    { id: 1, name: 'Item 1', price: 10, img: 'image-link-1' },
+    { id: 2, name: 'Item 2', price: 20, img: 'image-link-2' },
+    { id: 2, name: 'Item 2', price: 20, img: 'image-link-2' },
+    { id: 2, name: 'Item 2', price: 20, img: 'image-link-2' },
+    { id: 2, name: 'Item 2', price: 20, img: 'image-link-2' },
+    { id: 2, name: 'Item 2', price: 20, img: 'image-link-2' },
+    { id: 2, name: 'Item 2', price: 20, img: 'image-link-2' },
+    { id: 2, name: 'Item 2', price: 20, img: 'image-link-2' },
+    // Add more items as needed
+  ];
 
   return (
     <div>
-    <header>
-        <h1 style={{textAlign:'center', marginLeft:'40%'}}>Mens Wear Section</h1>
-    </header>
-    <section id="product">
-
-    {
-        items.map((item) => {
-            return (
-                <div class="product">
-                    <div class="product-info">
-                        <img src={item.img} alt='image'/>
-                        <h4 style={{margin:'20px'}}>{item.name}</h4>
-                        <h4 style={{margin:'20px'}}>{item.price}</h4>
-                    </div>
-                </div>
-
-            )
-        })
-    }
-            
-   
-</section>
+      <header>
+        <h1 style={{ textAlign: 'center', marginLeft: '40%' }}>Mens Wear Section</h1>
+      </header>
+      <section id="product">
+        {items.map((item) => {
+          return (
+            <div
+              key={item.id} // Added a unique key for each item
+              className="product"
+              style={{ backgroundImage: `url(${item.img})` }}
+            >
+              <div className="product-info">
+                <h4 style={{ margin: '20px' }}>{item.name}</h4>
+                <h4 style={{ margin: '20px' }}>{item.price}</h4>
+              </div>
+            </div>
+          );
+        })}
+      </section>
     </div>
-  )
+  );
 }
